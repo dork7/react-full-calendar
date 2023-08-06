1871189ce779db56b2398c726beecce930ec19b1
Hamza Mehmood <hamza.mahmood@zho.gov.ae>
2023-08-06 14:52:17 +0400

    refactoring

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Calender from '../components/Calender'
+import Calender from './components/SpeakerEvents/Calender'
 
 function App() {
   const [count, setCount] = useState(0)
